51b6785744634839c96f2ec45c9cc325e3b23ecc
Stefan Otte <stefan.otte@gmail.com>
2015-04-17 22:49:29 +0200

    Adjust docstrings, use google style.

--- pelper.py
+++ pelper.py
@@ -14,44 +14,61 @@ import time
 def pipe(data, *functions):
     """pipe data trough a pipeline of functions.
 
-    Think of unix pipes for python or elixir's pipes
+    Think of unix pipes or elixir's pipes.
+    ``pipe`` tries to be smart and automatically cerates partial functions if
+    you pass a tuple instead of a callable:
+
+    - ``pipe(data, (pow, 2, 5))`` --> ``pow(data, 2, 5)``.
+
+    - ``pipe(data, (sorted, {"reverse": True}))`` -->
+      ``sorted(data, reverse=True)``
+
+    Args:
+        data (whatever): the data that is passed into the first function.
+        functions (callables): functions that create the pipeline.
 
     Examples:
+        Of course you can use lambda functions:
 
-    Of course you can use lambda functions
-    >>> from math import ceil, sqrt
-    >>> pipe("2.1", float, ceil, int, lambda x: x*x, sqrt)
-    3.0
+        >>> from math import ceil, sqrt
+        >>> pipe("2.1", float, ceil, int, lambda x: x*x, sqrt)
+        3.0
 
-    Genrate your own partial functions with lambda
-    >>> pipe("2.1", float, ceil, int, lambda x: pow(x, 2), sqrt)
-    3.0
+        Genrate your own partial functions with lambda
 
-    >>> pipe("2.1", float, ceil, int, lambda x: pow(x, 2), sqrt)
-    3.0
+        >>> pipe("2.1", float, ceil, int, lambda x: pow(x, 2), sqrt)
+        3.0
 
-    There is a shortcut: tuples are interpreted as as
-    ``(functions, arguments, ...)``. The data passed from the previous function
-    is the first argument of the punction, followed by the arguments from the
-    tuple.
-    >>> pipe("2.1", float, ceil, int, (pow, 2), sqrt)
-    3.0
+        There is a shortcut: tuples are interpreted as as
+        ``(functions, arguments, ...)``.
+        The data passed from the previous function is the first argument of the
+        punction, followed by the arguments from the tuple.
 
-    To make this clearer ``pipe(3, (pow, 2))`` is equivalent to ``pow(3, 2)``.
-    Of course you can pass multiple arguments with the tuple
-    >>> pipe(3, (pow, 2, 8))  # pow(2, 3, 8) -> pow(2, 3) % 8
-    1
+        >>> pipe("2.1", float, ceil, int, (pow, 2), sqrt)
+        3.0
 
-    It can be convenient to use this notation if the function names are longer
-    >>> text = "atababsatsatsastatbadstssdhhhnbb"
-    >>> pipe(text,
-    ...      set,
-    ...      sorted)
-    ['a', 'b', 'd', 'h', 'n', 's', 't']
+        To make this clearer ``pipe(3, (pow, 2))`` is equivalent to
+        ``pow(3, 2)``.
+        Of course you can pass multiple arguments with the tuple:
 
-    It's also possible to use named arguments:
-    >>> pipe(text, set, (sorted, {"reverse": True}))
-    ['t', 's', 'n', 'h', 'd', 'b', 'a']
+        >>> pipe(3, (pow, 2, 8))  # pow(2, 3, 8) -> pow(2, 3) % 8
+        1
+
+        It can be convenient to use the following  notation if the function
+        names are longer:
+
+        >>> text = "atababsatsatsastatbadstssdhhhnbb"
+        >>> pipe(text,
+        ...      set,
+        ...      sorted)
+        ['a', 'b', 'd', 'h', 'n', 's', 't']
+
+        It's also possible to use named arguments:
+
+        >>> pipe(text,
+        ...      set,
+        ...      (sorted, {"reverse": True}))
+        ['t', 's', 'n', 'h', 'd', 'b', 'a']
 
     """
     for f in functions:
@@ -72,48 +89,57 @@ class print_duration(object):
 
     A "ContextDecorator" is a combination of decorator and context manager.
 
-    Warning:
-
-    This is not the best implementation of a context decorator.
-    Exception might get swallowed.
-
     Args:
-        msg (string): the message that is printed before the measured time
-            Default is None
+        msg (string, optional): the message that is printed before the measured
+            time.
+            Defaults to None.
+        out (callable, optional): if set ``out`` is called with the measured
+            duration.
+            This can be a logger for example. ``print`` is used if out is
+            ``None``. Defaults to ``None``.
+
+    Warning:
+        This is not the best implementation of a context decorator.
+        Exception might get swallowed.
 
     Examples:
+        Use print_duration as context manager:
 
-    >>> # use print_duration as context manager
-    >>> with print_duration():  #doctest: +ELLIPSIS
-    ...     # do something
-    ...     range(5)
-    [0, 1, 2, 3, 4]
-    Duration 0...s
+        >>> with print_duration():  #doctest: +ELLIPSIS
+        ...     # do something
+        ...     range(5)
+        [0, 1, 2, 3, 4]
+        Duration 0...s
 
-    >>> # specify a message
-    >>> with print_duration(msg="range test"):  #doctest: +ELLIPSIS
-    ...    range(5)
-    [0, 1, 2, 3, 4]
-    range test 0...s
+        Specify a message:
 
-    >>> # use your logger
-    >>> import logging
-    >>> log = logging.getLogger()
-    >>> with print_duration(out=log.warning):
-    ...    range(5)
-    [0, 1, 2, 3, 4]
+        >>> with print_duration(msg="range test"):  #doctest: +ELLIPSIS
+        ...    range(5)
+        [0, 1, 2, 3, 4]
+        range test 0...s
 
-    >>> # with message and logger
-    >>> with print_duration("range test", out=log.warning):
-    ...     range(5)
-    [0, 1, 2, 3, 4]
+        Use your logger:
 
-    >>> # use print_duration as decorator
-    >>> @print_duration("f took")
-    ... def f():
-    ...     range(5)
-    >>> f()  #doctest: +ELLIPSIS
-    f took 0...s
+        >>> import logging
+        >>> log = logging.getLogger()
+        >>> with print_duration(out=log.warning):
+        ...    range(5)
+        [0, 1, 2, 3, 4]
+
+        With message and logger:
+
+        >>> with print_duration("range test", out=log.warning):
+        ...     range(5)
+        [0, 1, 2, 3, 4]
+
+        Use print_duration as decorator:
+
+        >>> @print_duration("f took")
+        ... def f():
+        ...     range(5)
+        >>> f()  #doctest: +ELLIPSIS
+        f took 0...s
+
     """
     def __init__(self, msg=None, out=None):
         self.msg = msg
@@ -143,28 +169,32 @@ class print_duration(object):
 
 ###############################################################################
 def cache(f):
-    """Cache the results of f.
+    """Decorator: cache the results of f for the same parameters.
 
-    Note: Only use this with pure functions!
+    The decorated function is only called if the parameters differ from
+    previous calls.
+    Cache is really useful for recursive functions!
+
+    Warning:
+        Only use this with pure functions!
 
     Examples:
+        >>> # try the normal fib
+        >>> def fib(n):
+        ...     return 1 if n < 2 else fib(n-1) + fib(n-2)
+        >>> with print_duration():  #doctest: +ELLIPSIS
+        ...     fib(25)
+        121393
+        Duration 0...s
 
-    >>> # try the normal fib
-    >>> def fib(n):
-    ...     return 1 if n < 2 else fib(n-1) + fib(n-2)
-    >>> with print_duration():  #doctest: +ELLIPSIS
-    ...     fib(25)
-    121393
-    Duration 0...s
-
-    >>> # the cached version is much faster
-    >>> @cache
-    ... def fib(n):
-    ...     return 1 if n < 2 else fib(n-1) + fib(n-2)
-    >>> with print_duration():  #doctest: +ELLIPSIS
-    ...     fib(25)
-    121393
-    Duration 0...s
+        >>> # the cached version is much faster
+        >>> @cache
+        ... def fib(n):
+        ...     return 1 if n < 2 else fib(n-1) + fib(n-2)
+        >>> with print_duration():  #doctest: +ELLIPSIS
+        ...     fib(25)
+        121393
+        Duration 0...s
 
     """
     saved = {}
@@ -184,10 +214,12 @@ def cache(f):
 def ignored(*exception):
     """Context manager to ignore exceptions.
 
-    Example:
-    >>> with ignored(OSError):
-    ...     raise OSError  # this is ignored!
+    Args:
+        exception (Exception): the exception to ignore
 
+    Example:
+        >>> with ignored(OSError):
+        ...     raise OSError  # this is ignored!
 
     """
     try:
