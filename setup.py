edbbf00fc60c0cabe4adae32285e2598539a8579
Stefan Otte <stefan.otte@gmail.com>
2015-06-23 20:07:08 +0200

    new pelper structure in prep for pypi

--- setup.py
+++ setup.py
@@ -2,25 +2,30 @@
 # encoding: utf-8
 
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-def read(fname):
-    """Utility function to read the README file."""
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as f:
+    long_description = f.read()
 
 
 setup(
+    name="pelper",
+
+    version="0.0.2",
+
     description="pelper - python helper functions",
-    long_description=read("README.rst"),
+    long_description=long_description,
+
     author="Stefan Otte",
+    author_email="stefan.otte@gmail.com",
+
+    license="MIT",
+
     url="https://github.com/sotte/pelper",
     download_url="https://github.com/sotte/NAME",
-    author_email="stefan.otte@gmail.com",
-    version="0.0.1",
-    install_requires=["sphinxcontrib-napoleon", ],
-    packages=[],
-    scripts=["pelper.py"],
-    name="pelper",
-    license="MIT"
+
+    keywords="development heper decorator contextmanager",
+
+    packages=["pelper"],
+    # install_requires=["sphinxcontrib-napoleon", ],
 )
